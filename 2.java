cd307d39538c2a9f1f776cce5a9fcaba6d71679b
Gilles Patrick Ngondo <37463219+gillespatrick@users.noreply.github.com>
2024-05-12 20:28:48 -0400

    Update 2.java

--- 2.java
+++ 2.java
@@ -1,9 +1,7 @@
 This is for testing  propose
 Super let's go
-<<<<<<< HEAD
 This for feature branch 
 Also for testing
-=======
 This is for testing conflits
 From defect branch
 >>>>>>> caa3c0ea9ccecb95ece24ec5d527460e2102a362
